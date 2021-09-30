526b066d3cef792b7e2a9d5fc50957c1b2a3b2cd
Tim Flood <tflood9@gmail.com>
2021-09-30 15:47:58 -0400

    basic navigation

---
+++ app/javascript/packs/components/Navigation/Nav.js
@@ -0,0 +1,33 @@
+import React from "react";
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Workouts from "../Workouts";
+import Home from "../Home";
+
+const Nav = () => {
+  return (
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/workouts">Workouts</Link>
+            </li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route path="/workouts">
+            <Workouts />
+          </Route>
+          <Route path="/">
+            <Home />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
+  );
+};
+
+export default Nav;
---
+++ app/javascript/packs/components/Workouts.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Workouts = () => {
+  return (
+    <div>
+      <p>Here's where the workout list will be!</p>
+    </div>
+  );
+};
+
+export default Workouts;
